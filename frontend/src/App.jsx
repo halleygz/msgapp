4503f558dbcb868700fe341f8759e0f7edc30d29
halleygz <halleluya2013idone@gmail.com>
2024-05-30 10:17:15 +0300

    made the SignUp page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import './index.css'
 import Login from './pages/login/Login'
+import Signup from './pages/signup/Signup'
 
 function App() {
 
   return (
     <div className="p-4 h-screen flex items-center justify-center">
-      <Login/>
+      <Signup/>
     </div>
   )
 }

b257cc044da75a9a31cff8fbdbdc192a7c0ac072
halleygz <halleluya2013idone@gmail.com>
2024-06-01 21:51:42 +0300

    implemented signUp functionality from frontend.
    also added toaster

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,20 @@
+import { Route, Routes } from 'react-router-dom'
 import './index.css'
 import Home from './pages/home/Home'
 import Login from './pages/login/Login'
 import Signup from './pages/signup/Signup'
+import {Toaster} from 'react-hot-toast'
 
 function App() {
 
   return (
     <div className="p-4 h-screen flex items-center justify-center">
-      <Home/>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+      </Routes>
+      <Toaster/>
     </div>
   )
 }
--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
@@ -21,9 +22,9 @@ const Login = () => {
             </label>
             <input type="password" placeholder="Enter username" className="w-full input input-bordered h-10"/>
           </div>
-          <a href='#' className='text-sm  hover:underline hover:text-blue-600 mt-2 inline-block'>
+          <Link to='/signup' className='text-sm  hover:underline hover:text-blue-600 mt-2 inline-block'>
 						{"Don't"} have an account?
-					</a>
+					</Link>
           <div>
  						<button className='btn btn-block btn-sm mt-2'>Login</button>
 				</div>
